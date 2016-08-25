c308b5314a964b39fbccac855cc8cb09db6cdeb2
erabbit <rabbitom@gmail.com>
2016-08-25 15:24:03 +0800

    show characteristics

--- src-iOS/BLESensor/DeviceServicesViewController.m
+++ src-iOS/BLESensor/DeviceServicesViewController.m
@@ -13,6 +13,7 @@
 @interface DeviceServicesViewController ()
 {
     BLEDevice *_devcie;
+    BOOL showCharacteristics;
 }
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segments;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
@@ -30,6 +31,7 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    showCharacteristics = NO;
     DLog("");
 }
 
@@ -43,33 +45,72 @@
         [self.tableView reloadData];
 }
 
+- (IBAction)toggleServicesAndCharacteristics:(id)sender {
+    showCharacteristics = (self.segments.selectedSegmentIndex == 1);
+    [self.tableView reloadData];
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    if(showCharacteristics)
+        return self.device.peripheral.services.count;
+    else
+        return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.device.peripheral.services.count;
+    if(showCharacteristics) {
+        CBService *service = self.device.peripheral.services[section];
+        return service.characteristics.count;
+    }
+    else
+        return self.device.peripheral.services.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ServiceItem" forIndexPath:indexPath];
-    
-    UILabel *serviceName = [cell viewWithTag:1];
-    UILabel *serviceUUID = [cell viewWithTag:2];
-    UILabel *serviceInfo = [cell viewWithTag:3];
-    
-    CBService *service = self.device.peripheral.services[indexPath.row];
-    NSString *_serviceName = [BLEUtility serviceName:service.UUID];
-    serviceName.text = STRING_BY_DEFAULT(_serviceName, @"Unknown Service");
-    serviceUUID.text = [service.UUID UUIDString];
-    serviceInfo.text = service.isPrimary ? @"PRIMARY" : @"SECONDARY";
-    
-    CGRect infoRect = serviceInfo.frame;
-    DLog(@"service info: x=%f, y=%f, w=%f, h=%f", infoRect.origin.x, infoRect.origin.y, infoRect.size.width, infoRect.size.height);
-    
-    return cell;
+    if(showCharacteristics) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CharacteristicItem" forIndexPath:indexPath];
+        
+        UILabel *name = [cell viewWithTag:1];
+        UILabel *UUID = [cell viewWithTag:2];
+        UILabel *info = [cell viewWithTag:3];
+        
+        CBService *service = self.device.peripheral.services[indexPath.section];
+        CBCharacteristic *characteristic = service.characteristics[indexPath.row];
+        name.text = @"Unknown Characteristic";
+        UUID.text = [characteristic.UUID UUIDString];
+        CBCharacteristicProperties properties = characteristic.properties;
+        NSMutableArray *propertyArray = [NSMutableArray array];
+        if(properties & CBCharacteristicPropertyBroadcast)[propertyArray addObject:@"Broadcast"];
+        if(properties & CBCharacteristicPropertyRead)[propertyArray addObject:@"Read"];
+        if(properties & CBCharacteristicPropertyWriteWithoutResponse)[propertyArray addObject:@"WriteWithoutResponse"];
+        if(properties & CBCharacteristicPropertyWrite)[propertyArray addObject:@"Write"];
+        if(properties & CBCharacteristicPropertyNotify)[propertyArray addObject:@"Notify"];
+        if(properties & CBCharacteristicPropertyIndicate)[propertyArray addObject:@"Indicate"];
+        if(properties & CBCharacteristicPropertyAuthenticatedSignedWrites)[propertyArray addObject:@"AuthenticatedSignedWrites"];
+        if(properties & CBCharacteristicPropertyExtendedProperties)[propertyArray addObject:@"ExtendedProperties"];
+        if(properties & CBCharacteristicPropertyNotifyEncryptionRequired)[propertyArray addObject:@"NotifyEncryptionRequired"];
+        if(properties & CBCharacteristicPropertyIndicateEncryptionRequired)[propertyArray addObject:@"IndicateEncryptionRequired"];
+        info.text = [propertyArray componentsJoinedByString:@" | "];
+        
+        return cell;
+    }
+    else {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ServiceItem" forIndexPath:indexPath];
+        
+        UILabel *serviceName = [cell viewWithTag:1];
+        UILabel *serviceUUID = [cell viewWithTag:2];
+        UILabel *serviceInfo = [cell viewWithTag:3];
+        
+        CBService *service = self.device.peripheral.services[indexPath.row];
+        NSString *_serviceName = [BLEUtility serviceName:service.UUID];
+        serviceName.text = STRING_BY_DEFAULT(_serviceName, @"Unknown Service");
+        serviceUUID.text = [service.UUID UUIDString];
+        serviceInfo.text = service.isPrimary ? @"PRIMARY" : @"SECONDARY";
+        
+        return cell;
+    }
 }
 
 /*
